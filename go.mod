e08015465f64739b4c5a5cd1d5e156107a11267d
fernando8franco <francoduranluisfernando@gmail.com>
2025-11-14 11:40:19 -0600

    fileservers

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fernando8franco/http-server-golang
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	serverMux := http.NewServeMux()
+	serverMux.Handle("/", http.FileServer(http.Dir(".")))
+
+	server := http.Server{
+		Handler: serverMux,
+		Addr:    ":8080",
+	}
+
+	server.ListenAndServe()
+}
