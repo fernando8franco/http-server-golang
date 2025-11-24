5ea8fcf705802995e15343af10f7a6f20a570ebe
fernando8franco <francoduranluisfernando@gmail.com>
2025-11-24 15:04:38 -0600

    stateful handlers && routing

--- main.go
+++ main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	serverMux := http.NewServeMux()
-	serverMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	serverMux.Handle("/app/assets/logo.png", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	serverMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	apiCfg := &apiConfig{}
+
+	serverMux.Handle("GET /app", apiCfg.middlewareMetricsInc(handler))
+	serverMux.Handle("GET /app/assets/logo.png", apiCfg.middlewareMetricsInc(handler))
+	serverMux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
+	serverMux.HandleFunc("GET /metrics", apiCfg.metrics)
+	serverMux.HandleFunc("POST /reset", apiCfg.reset)
 
 	server := http.Server{
 		Handler: serverMux,
@@ -20,4 +31,22 @@ func main() {
 	}
 
 	server.ListenAndServe()
+}
+
+func (ac *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ac.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (ac *apiConfig) metrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %v", ac.fileserverHits.Load())
+}
+
+func (ac *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	ac.fileserverHits.Store(0)
 }
