d9c9b5fe2572c7a14ee89a34f23b5f6446a178fb
fernando8franco <francoduranluisfernando@gmail.com>
2025-12-15 12:52:40 -0600

    validate token code and tests fixed

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,11 +1,18 @@
 package auth
 
 import (
+	"errors"
 	"time"
 
 	"github.com/alexedwards/argon2id"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
+)
+
+type TokenType string
+
+const (
+	TokenTypeAccess TokenType = "chirpy"
 )
 
 func HashPassword(password string) (string, error) {
@@ -27,8 +34,8 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 }
 
 func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
-	claims := &jwt.RegisteredClaims{
-		Issuer:    "chirpy",
+	claims := jwt.RegisteredClaims{
+		Issuer:    string(TokenTypeAccess),
 		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
 		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn).UTC()),
 		Subject:   userID.String(),
@@ -51,6 +58,15 @@ func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
 	}
 
 	cl := token.Claims
+
+	issuer, err := cl.GetIssuer()
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	if issuer != string(TokenTypeAccess) {
+		return uuid.Nil, errors.New("the issuer is not valid")
+	}
 
 	subject, err := cl.GetSubject()
 	if err != nil {
--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -7,36 +7,50 @@ import (
 	"github.com/google/uuid"
 )
 
-func TestJWT(t *testing.T) {
-	id1, id2 := uuid.New(), uuid.New()
+func TestValidateJWT(t *testing.T) {
+	userID := uuid.New()
+	validToken, _ := MakeJWT(userID, "secret", time.Hour)
+
 	tests := []struct {
-		name      string
-		userId    uuid.UUID
-		secret    string
-		expiresIn time.Duration
-		want      uuid.UUID
-		wantErr   bool
+		name        string
+		tokenString string
+		tokenSecret string
+		wantUserId  uuid.UUID
+		wantErr     bool
 	}{
-		{"success jwt validation", id1, "test123", 100 * time.Second, id1, false},
-		{"wrong jwt validation", id2, "thisIsASecret", 1 * time.Second, uuid.Nil, true},
+		{
+			"Valid token",
+			validToken,
+			"secret",
+			userID,
+			false,
+		},
+		{
+			"Invalid token",
+			"invalid-token-string",
+			"secret",
+			uuid.Nil,
+			true,
+		},
+		{
+			"Wrong secret",
+			validToken,
+			"wrong_secret",
+			uuid.Nil,
+			true,
+		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			token, err := MakeJWT(test.userId, test.secret, test.expiresIn)
-			if err != nil {
-				t.Errorf("unexpected error got %v", err)
-			}
-
-			time.Sleep(2 * time.Second)
-
-			id, err := ValidateJWT(token, test.secret)
+			userId, err := ValidateJWT(test.tokenString, test.tokenSecret)
 			if (err != nil) != test.wantErr {
-				t.Errorf("expected error %v, got %v", test.wantErr, err)
+				t.Errorf("ValidateJWT()\nerror = %v\nwantErr = %v", test.wantErr, err)
+				return
 			}
 
-			if id != test.want {
-				t.Errorf("expected %v, got %v", test.want, id)
+			if userId != test.wantUserId {
+				t.Errorf("ValidateJWT()\nuserId = %v\nwantId = %v", test.wantErr, err)
 			}
 		})
 	}
